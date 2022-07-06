bae045b02d9810ae20847d1ec7f6555d6548a13b
ShiraandNessi <schoolbusproject2022@gmail.comgit config --global user.email schoolbusproject2022@gmail.comgit config --global user.email schoolbusproject2022@gmail.com>
2022-07-06 16:35:01 +0300

    array promise works!!

--- week_1_project/promise.js
+++ week_1_project/promise.js
@@ -1,10 +1,48 @@
-// window.onPageLoad = randoNumber();
-let promise = new Promise(function(resolve, reject) {
-    setTimeout(() => {resolve(`the rand anser is ${Math.floor(Math.random() *10)}`),
-    reject(new Error("not valid")),3000})
-  });
-  
-  promise.then(
-    resolve => alert(resolve), 
-    reject => alert(reject) 
-  );
+let strings = ["E","F","G","H"];
+var er =true;
+
+let promise = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        resolve(`the rand anser is ${Math.floor(Math.random() * 10)}`),
+        reject(new Error("not valid")), 3000
+    })
+});
+promise.then(
+    resolve => alert(resolve),
+    reject => alert(reject)
+);
+
+
+let promise2 = new Promise(function (resolve, reject) {
+    let lowerStrings = toLower(strings);
+    if (lowerStrings!=false) {
+        resolve(lowerStrings);
+    } else {
+        reject("invalid array");
+    }
+})
+promise2.then(
+    function (resolve) {
+        resolve = resolve.sort();
+        alert((resolve.toString()))
+    }
+    ,
+    function (reject) {
+        alert(reject)
+    }
+)
+
+function toLower(array) {
+    array.forEach((ele, i) => {
+        if (typeof (ele) == typeof ("")) {
+            array[i] = array[i].toLowerCase();
+        }
+        else {
+           er = false;
+        }
+    });
+     if (er)
+        return array;
+     else
+       return er;
+}
